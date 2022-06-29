710358ba72fe47cc3153ce1f3d68daa0e42e7b83
hariprasathsharama <hariprasathsharma956@gmail.com>
2022-06-29 22:00:32 +0530

    try more

---
+++ Evilno.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class Evilno {
+    public static void main(String[] args) {
+        Scanner s=new Scanner(System.in);
+        int n=s.nextInt();
+        s.close();
+        int sum=0;
+        while(n>0){
+            int r=n%2;
+             sum=sum+r;
+            n=n/2;
+        }
+       
+        if(sum%2==0){
+            System.out.println("it is evil number");
+        }
+        else{
+            System.out.println("it is odious number");
+        }
+    }
+}
